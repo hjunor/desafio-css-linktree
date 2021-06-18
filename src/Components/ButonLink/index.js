4213c7e4d74598e745059f58980648ebbf941c5f
Heberth Junor <hjunor@gmail.com>
2021-06-18 00:02:46 -0300

    refactor: blank new link

--- src/Components/ButonLink/index.js
+++ src/Components/ButonLink/index.js
@@ -2,13 +2,30 @@ import React from "react";
 
 import { GetLink } from "./styles";
 
-const ButtonLink = ({ color, href, name, handler, type, width, height, min_width , background }) => {
-  
+const ButtonLink = ({
+  color,
+  href,
+  name,
+  handler,
+  type,
+  width,
+  height,
+  min_width,
+  background,
+}) => {
   return (
-    <GetLink min_width={min_width} width={width} height={height} href={href} color={color} background={background}>
+    <GetLink
+      target="_blank"
+      min_width={min_width}
+      width={width}
+      height={height}
+      href={href}
+      color={color}
+      background={background}
+    >
       {name}
     </GetLink>
   );
 };
 
-export  default ButtonLink;
+export default ButtonLink;
